263ec98325ba9862a2a6b9dcf1d0613f0bd834e1
samrauer <samrauer@umich.edu>
2025-08-25 22:33:04 -0400

    functions completed

--- exercises/02_functions/functions2.rs
+++ exercises/02_functions/functions2.rs
@@ -1,10 +1,14 @@
 // TODO: Add the missing type of the argument `num` after the colon `:`.
-fn call_me(num:) {
-    for i in 0..num {
-        println!("Ring! Call number {}", i + 1);
-    }
-}
 
 fn main() {
     call_me(3);
+}
+
+
+fn call_me(num:u32) {
+    let mut x = 0;
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
+        x += i;
+    }
 }
